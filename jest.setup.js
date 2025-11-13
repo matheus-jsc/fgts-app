2c183625e5a661f68af915a6d8cdbf604283bb84
matheus-jsc <matheus.costa@popcode.com.br>
2025-11-13 01:16:19 -0300

    chore: tirando dependencias

--- jest.setup.js
+++ jest.setup.js
@@ -1,17 +1,5 @@
 import '@testing-library/jest-native/extend-expect';
 
-jest.mock('@react-native-async-storage/async-storage', () => ({
-  setItem: jest.fn().mockImplementation(() => {
-    return Promise.resolve()
-  }),
-  getItem: jest.fn().mockImplementation(() => {
-    return Promise.resolve()
-  }),
-  removeItem: jest.fn().mockImplementation(() => {
-    return Promise.resolve()
-  })
-}))
-
 jest.mock('react-native-safe-area-context', () => {
   const inset = { top: 0, right: 0, bottom: 0, left: 0 };
   return {
